b2504f07f736684211981a10d7bb91005e4fee78
Ricko <Rhaikal101@gmail.com>
2025-11-25 09:05:07 +0700

    feat: update detection/image/predict to return Grad-CAM overlay and include predicted label & score in headers

---
+++ app/modules/detection_explainer.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+
+
+def generate_explanation_heatmap(model, img_array, layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs], [model.get_layer(layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(img_array, training=False)
+
+        if isinstance(predictions, (list, tuple)):
+            predictions = predictions[0]
+
+        class_channel = predictions[:, 0]
+
+    grads = tape.gradient(class_channel, conv_outputs)
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    conv_outputs = conv_outputs[0]
+    heatmap = tf.reduce_mean(tf.multiply(pooled_grads, conv_outputs), axis=-1)
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    heatmap = heatmap.numpy()
+
+    return heatmap
+
+
+def generate_overlay(img_path, heatmap, alpha=0.4):
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (299, 299))
+
+    heatmap_resized = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+
+    heatmap_colored = np.uint8(255 * heatmap_resized)
+    heatmap_colored = cv2.applyColorMap(heatmap_colored, cv2.COLORMAP_JET)
+
+    superimposed_img = cv2.addWeighted(img, 1 - alpha, heatmap_colored, alpha, 0)
+
+    # superimposed_img = cv2.cvtColor(superimposed_img, cv2.COLOR_BGR2RGB)
+
+    success, buffer = cv2.imencode(".png", superimposed_img)
+    if not success:
+        raise ValueError("Image encoding failed")
+
+    return buffer.tobytes()
--- app/routers/detection.py
+++ app/routers/detection.py
@@ -1,6 +1,8 @@
+import io
+
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, UploadFile
+from fastapi import APIRouter, Depends, UploadFile, Response
 
 from app.dependencies.auth import require_auth
 from app.models import User
@@ -12,6 +14,10 @@ from app.modules.detection import (
     preprocess_image,
     save_upload_file,
 )
+from app.modules.detection_explainer import (
+    generate_explanation_heatmap,
+    generate_overlay,
+)
 
 router = APIRouter()
 
@@ -20,13 +26,32 @@ router = APIRouter()
 async def predict(
     file: UploadFile,
     user: Annotated[User, Depends(require_auth)],
-) -> dict:
+) -> Response:
     filepath = save_upload_file(file)
 
     try:
         img_array = preprocess_image(filepath)
+
+        # prediction
         model = load_prediction_model()
         prediction = predict_image(img_array, model)
-        return get_prediction_result(prediction)
+        result = get_prediction_result(prediction)
+
+        # explain prediction
+        heatmap = generate_explanation_heatmap(
+            model, img_array, layer_name="custom_block1_conv"
+        )
+        overlay_bytes = generate_overlay(filepath, heatmap)
+
+        return Response(
+            content=overlay_bytes,
+            headers={
+                "X-Predicted-Class": result["predicted_class"],
+                "X-Positive-Score": str(result["predicted_score"]["positive"]),
+                "X-Negative-Score": str(result["predicted_score"]["negative"]),
+            },
+            media_type="image/png",
+        )
+
     finally:
         cleanup_file(filepath)
